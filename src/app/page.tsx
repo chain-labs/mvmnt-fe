09c34da36dd759714b39ff6f2b334ebb29e9361b
Yash <91452674+xinkin@users.noreply.github.com>
2024-06-19 03:43:53 +0530

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,29 @@ export default function Home() {
 
   return (
     <main className="flex flex-col justify-center h-screen items-center gap-20">
-      {!authenticated && (
+      {!authenticated ? (
         <button
           className="bg-blue-400 p-3 w-[170px] rounded-xl"
           onClick={onLogIn}
         >
           Log In
         </button>
-      )}
-      {authenticated && <div>Smart Account Address: {smartAccountAddress}</div>}
-      {authenticated && (
-        <button
-          className="bg-purple-500 p-3 w-[170px] rounded-xl"
-          onClick={signMessage}
-        >
-          Sign Message
-        </button>
-      )}
-      {authenticated && (
-        <button
-          className="bg-red-500 p-3 w-[170px] rounded-xl"
-          onClick={logout}
-        >
-          Log Out
-        </button>
+      ) : (
+        <>
+          <div>Smart Account Address: {smartAccountAddress}</div>
+          <button
+            className="bg-purple-500 p-3 w-[170px] rounded-xl"
+            onClick={signMessage}
+          >
+            Sign Message
+          </button>
+          <button
+            className="bg-red-500 p-3 w-[170px] rounded-xl"
+            onClick={logout}
+          >
+            Log Out
+          </button>
+        </>
       )}
     </main>
   );
