9db1e3308ec2929a28891b9cb1924b6ece93ecf2
Yash <91452674+xinkin@users.noreply.github.com>
2024-06-19 01:36:07 +0530

    smart wallets via privy & biconomy

---
+++ src/app/privyprovider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId="clxklxwn200kfdbk39te9ie6a"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: "light",
+          accentColor: "#676FFF",
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
