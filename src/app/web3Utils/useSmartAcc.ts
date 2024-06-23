3a9b26b8d8ec306bd15c0ba439b390d10d6c2bc8
Yash <91452674+xinkin@users.noreply.github.com>
2024-06-24 04:05:07 +0530

    verification checks

--- src/app/web3Utils/useSmartAcc.ts
+++ src/app/web3Utils/useSmartAcc.ts
@@ -6,7 +6,6 @@ import {
 } from "@privy-io/react-auth";
 import { useEffect, useState } from "react";
 import { connect, signMessage } from "./accUtils";
-import { log } from "console";
 
 export const useSmartAcc = () => {
   const [smartAccountAddress, setSmartAccountAddress] = useState<string | null>(
@@ -17,6 +16,7 @@ export const useSmartAcc = () => {
   const { logout, authenticated, linkPhone, createWallet, user } = usePrivy();
   const { ready, wallets } = useWallets();
 
+  // Delete user if verification condition is not met
   const handleDelete = async (userId: string) => {
     try {
       const response = await fetch("/api/userDelete", {
@@ -33,9 +33,11 @@ export const useSmartAcc = () => {
     }
   };
 
+  //callbacks for login flow
   const { login } = useLogin({
     onComplete: (user) => {
       console.log("user", user);
+      //check if user has verified email, if not delete user
       if (!user.email) {
         console.log(
           "Your Account is not verified yet. Please verify your email"
@@ -43,28 +45,49 @@ export const useSmartAcc = () => {
         handleDelete(user.id);
         logout();
       }
+      //check if user has registered, if not delete user
+      if (!user.wallet) {
+        console.log("User not registered. Please register first.");
+        handleDelete(user.id);
+        logout();
+      }
     },
   });
 
+  // console.log("user", user?.createdAt);
+  // const createdAt = new Date(user?.createdAt ?? "");
+  // const d = new Date();
+  // console.log(createdAt < d);
+
+  //registration flow, created wallet for user when registering
   useEffect(() => {
     if (authenticated && ready) {
-      if (user?.wallet?.walletClientType !== "privy") {
-        createWallet();
+      if (wallets.length === 0) {
+        console.log("wallets", wallets);
+        if (!user?.wallet) {
+          setTimeout(async () => {
+            try {
+              await createWallet();
+              console.log("Wallet Created");
+            } catch (error) {
+              console.log("Error creating wallet:", error);
+              logout();
+            }
+          });
+        }
       }
+      // else if (new Date() > new Date(user?.createdAt ?? "")) {
+      //   console.log("User is already registered");
+      //   logout();
+      // }
     }
-  }, [useLoginWithEmail]);
+  }, [loginWithCode]);
 
   useEffect(() => {
     if (authenticated) {
       connect(wallets, setSmartAccountAddress);
     }
   }, [wallets]);
-
-  // useEffect(() => {
-  //   if (authenticated && ready) {
-
-  //   }
-  // }, [login]);
 
   return {
     sendCode,
